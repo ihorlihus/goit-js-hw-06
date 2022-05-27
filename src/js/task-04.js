b671a6d6c35039858e5ef490ce0a47b5f950ca2f
ihorlihus <ligusigor@ukr.net>
2022-05-27 17:54:35 +0300

    task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,6 +1,24 @@
 
+const counterValueEl = document.querySelector('#value');
+const buttonDescr = document.querySelector('[data-action="decrement"]');
+const buttonIncr = document.querySelector('[data-action="increment"]');
 
-const counterValue = 0;
+const counter = {
+    value: 0,
+    decrement() {
+        this.value -= 1;
+    },
+    increment() {
+        this.value += 1;
+    },
+}
 
-const buttonIncr = document.querySelector()
-const buttonDescr = document.querySelector()
+buttonDescr.addEventListener("click", () => {    
+    counter.decrement();
+    counterValueEl.textContent = counter.value;
+});
+
+buttonIncr.addEventListener("click", () => {
+    counter.increment();
+    counterValueEl.textContent = counter.value;
+});
