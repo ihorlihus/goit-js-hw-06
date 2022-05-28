cc49e99ffea73a4356bdc247f60c43eb78ebf1bf
ihorlihus <ligusigor@ukr.net>
2022-05-28 14:16:33 +0300

    task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,25 +1,15 @@
 
 const textInput = document.querySelector("input");
-const setFocusBtn = document.querySelector('[data-action="set"]');
-const removeFocusBtn = document.querySelector('[data-action="remove"]');
-const currentValue = textInput.dataset.length;
-// const greenColor = document.querySelector('#validation-input.valid');
-textInput.id = '#validation-input validation-input.valid'
+const correctValue = textInput.dataset.length;
 
-console.log(textInput)
+textInput.addEventListener('blur', checkInputValue);
 
-// setFocusBtn.addEventListener("click", () => {
-//     textInput.focus();
-// });
-
-// removeFocusBtn.addEventListener("click", () => {
-//     textInput.blur();
-// });
-
-// textInput.addEventListener("focus", () => {
-//     textInput.value = "This input has focus";
-// });
-
-// textInput.addEventListener("blur", () => {
-//     textInput.textContent.length = "";
-// });
+function checkInputValue() {
+    const { value, classList } = textInput;
+    if (value.trim().length != correctValue) {
+        classList.add('invalid');
+    } else {
+        classList.remove('invalid');
+        classList.add('valid');
+    }
+}
