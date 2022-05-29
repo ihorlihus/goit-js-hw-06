860c01b0343a777b91f940a30bb48575ce36c9da
ihorlihus <ligusigor@ukr.net>
2022-05-29 22:20:19 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const windowBgColorEl = document.body
+const colorEl = windowBgColorEl.querySelector('.color');
+const buttonEl = windowBgColorEl.querySelector('.change-color');
+
+buttonEl.addEventListener('click', changeBgColor)
+
+function changeBgColor(event) {
+  if (event) {
+    colorEl.textContent = getRandomHexColor();
+
+    windowBgColorEl.style.backgroundColor = colorEl.textContent;
+  }
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,31 @@
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const controlsEl = document.querySelector('#controls')
+const inputEl = controlsEl.firstElementChild;
+const btnCreate = inputEl.nextElementSibling;
+const buttonDestroy = btnCreate.nextElementSibling;
+const inBoxesEl = document.querySelector('#boxes');
+
+let nombers;
+
+inputEl.addEventListener('change', (event) => {
+  nombers = event.currentTarget.value;
+  
+  btnCreate.addEventListener('click', createBoxes);
+})
+
+function createBoxes(amount) {
+color = getRandomHexColor()
+
+  const koubs = `<div width="30px" height="30px" color="${color}"></div>`
+  inBoxesEl.insertAdjacentHTML("beforeend", koubs);
+  console.log(inBoxesEl)
+}
+
+console.log(inBoxesEl)
