f0e3c22831b682000e197c6538d44c4ef005dc03
ihorlihus <ligusigor@ukr.net>
2022-05-29 14:29:17 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+    }
+    
+    const elements = {
+        Email: email.value,
+        Password: password.value
+    }
+
+    console.log(elements);
+
+    event.currentTarget.reset();
+}
