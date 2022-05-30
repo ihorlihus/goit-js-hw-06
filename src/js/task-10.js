6c3ba6c8aa490664049b9400881e52f2cfcecfe6
ihorlihus <ligusigor@ukr.net>
2022-05-30 16:14:57 +0300

    update task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -12,20 +12,30 @@ const btnCreate = inputEl.nextElementSibling;
 const buttonDestroy = btnCreate.nextElementSibling;
 const inBoxesEl = document.querySelector('#boxes');
 
-let nombers;
 
-inputEl.addEventListener('change', (event) => {
-  nombers = event.currentTarget.value;
-  
-  btnCreate.addEventListener('click', createBoxes);
+
+inputEl.addEventListener('input', (e) => {
+  const boxesNum = e.currentTarget.value;
+  console.log(boxesNum);
+  btnCreate.addEventListener('change', createBoxes(boxesNum));
 })
 
 function createBoxes(amount) {
-color = getRandomHexColor()
-
-  const koubs = `<div width="30px" height="30px" color="${color}"></div>`
-  inBoxesEl.insertAdjacentHTML("beforeend", koubs);
-  console.log(inBoxesEl)
+  const items = [];
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+    const item = document.createElement("div");
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.style.width = '30px';
+    item.style.height = '30px';
+    items.push(item);
+  }
+  inBoxesEl.append(...items);
 }
+
+buttonDestroy.addEventListener('click', () => {
+  inBoxesEl.innerHTML = '';
+})
 
 console.log(inBoxesEl)
