f39e11526267e5b0561668328ee2de965b4d679b
ihorlihus <ligusigor@ukr.net>
2022-06-09 22:27:36 +0300

    after

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,12 +1,12 @@
 
 const textInput = document.querySelector("input");
-const correctValue = textInput.dataset.length;
+const correctValue = Number(textInput.dataset.length);
 
 textInput.addEventListener('blur', checkInputValue);
 
 function checkInputValue() {
     const { value, classList } = textInput;
-    if (value.trim().length != correctValue) {
+    if (value.trim().length !== correctValue) {
         classList.add('invalid');
     } else {
         classList.remove('invalid');
